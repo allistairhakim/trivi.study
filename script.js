f01a6d1402bcb664cbd8c6141195617e9590b386
Allistair Hakim <allistair.hakim@gmail.com>
2025-02-08 21:15:31 -0500

    Changed URL

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ async function submitTopic() {
 
   try {
       // First request
-      const response = await fetch('http://vmserver:5000/generate-questions', {
+      const response = await fetch('http://trivy-study.loca.lt/generate-questions', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
@@ -100,7 +100,7 @@ async function submitTopic() {
       const data = await response.json();
 
       // Second request
-      const response1 = await fetch('http://vmserver:5000/generate-upgrades', {
+      const response1 = await fetch('http://trivy-study.loca.lt/generate-upgrades', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
